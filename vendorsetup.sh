43498e55b4382a60b192bc7718cfd38010f665c0
BensolBiru <89448703+Ariden24@users.noreply.github.com>
2024-07-12 23:42:46 +0700

    Create vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,27 @@
+# Some about us
+export FOX_VERSION="R12.1"
+export OF_MAINTAINER="BensolBiru"
+
+# Build environment stuffs
+export FOX_BUILD_DEVICE="MI 6X"
+export ALLOW_MISSING_DEPENDENCIES=true
+export BUILD_USERNAME="ariden24"
+export BUILD_HOSTNAME="BensolBiru"
+#export OF_MAINTAINER_AVATAR
+
+# We have a/b partitions
+export FOX_AB_DEVICE=0
+export FOX_VIRTUAL_AB_DEVICE=0
+
+# Dont install APPMANAGER
+export FOX_DISABLE_APP_MANAGER=1
+
+# OTA for custom ROMs
+export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
+
+# Dont install AROMAFM
+export FOX_DELETE_AROMAFM=1
+
+# Add some extras
+export OF_ENABLE_LPTOOLS=1
+export FOX_USE_NANO_EDITOR=1
